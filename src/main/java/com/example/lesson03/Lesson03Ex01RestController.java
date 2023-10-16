fd1d96e9776c634259b23a4c298f354e097acb3e
xd1124 <sjtand14@gmail.com>
2023-10-16 15:34:23 +0900

    3강 Mybatis SELECT

--- src/main/java/com/example/lesson03/Lesson03Ex01RestController.java
+++ src/main/java/com/example/lesson03/Lesson03Ex01RestController.java
@@ -1,16 +1,29 @@
 package com.example.lesson03;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.lesson03.bo.ReviewBO;
 import com.example.lesson03.domain.Review;
 
 @RestController
 public class Lesson03Ex01RestController {
 	
+	@Autowired
+	public ReviewBO reviewBO;
+	
 	// URL : http://localhost/lesson03/ex01
+	// URL : http://localhost/lesson03/ex01?id=5
 	@RequestMapping("/lesson03/ex01")
-	public Review ex01() {
-		return ;
+	public Review ex01(
+//					@RequestParam("id") int id // 필수 파라미터
+//					@RequestParam(value="id") int id // 필수
+//					@RequestParam(value="id", required=true) int id // 필수
+//					@RequestParam(value="id", required=false) Integer id // 비필수
+					@RequestParam(value="id", defaultValue="1") int id // 비필수, 디폴트 1
+			) {
+		return reviewBO.getReview(id);
 	}
 }
--- src/main/java/com/example/lesson03/bo/ReviewBO.java
+++ src/main/java/com/example/lesson03/bo/ReviewBO.java
@@ -1,15 +1,20 @@
 package com.example.lesson03.bo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.lesson03.domain.Review;
+import com.example.lesson03.mapper.ReviewMapper;
 
 @Service
 public class ReviewBO {
 	
+	@Autowired
+	public ReviewMapper reviewmapper;
+	
 	// input : 
 	// output : Review(단건)
-	public Review getReview() {
-		return ;
+	public Review getReview(int id) {
+		return reviewmapper.selectReview(id);
 	}
 }
